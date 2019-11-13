14be808e04e6b5870964d0c54d3802f0945d7fe8
kfrysiak <konrad.frysiak@gmail.com>
2019-11-13 22:04:50 +0100

    rating fix; select fix

--- src/modules/rating/components/rating.ts
+++ src/modules/rating/components/rating.ts
@@ -1,54 +1,70 @@
-import { Component, Directive, Input, Output, EventEmitter, HostBinding, HostListener } from "@angular/core";
-import { ICustomValueAccessorHost, customValueAccessorFactory, CustomValueAccessor } from "../../../misc/util/internal";
+import {
+    Component,
+    Directive,
+    Input,
+    Output,
+    EventEmitter,
+    HostBinding,
+    HostListener
+} from '@angular/core';
+import {
+    ICustomValueAccessorHost,
+    customValueAccessorFactory,
+    CustomValueAccessor
+} from '../../../misc/util/internal';
 
 @Component({
-    selector: "sui-rating",
+    selector: 'sui-rating',
     template: `
-<i class="icon"
-   *ngFor="let icon of icons; let i = index"
-   (mouseover)="onMouseover(i)"
-   (click)="onClick(i)"
-   [class.selected]="hoveredIndex >= i && !isReadonly"
-   [class.active]="value > i">
-</i>
-`,
-    styles: [`
-:host.read-only .icon {
-    cursor: auto
-}
-`]
+        <i
+            class="icon"
+            *ngFor="let icon of icons; let i = index"
+            (mouseover)="onMouseover(i)"
+            (click)="onClick(i)"
+            [class.selected]="hoveredIndex >= i && !isReadonly"
+            [class.active]="value > i"
+        >
+        </i>
+    `,
+    styles: [
+        `
+            :host.read-only .icon {
+                cursor: auto;
+            }
+        `
+    ]
 })
 export class SuiRating implements ICustomValueAccessorHost<number> {
-    @HostBinding("class.ui")
-    @HostBinding("class.rating")
-    public readonly hasClasses:boolean;
+    @HostBinding('class.ui')
+    @HostBinding('class.rating')
+    public readonly hasClasses: boolean;
 
-    public value:number;
+    public value: number;
 
     @Output()
-    public valueChange:EventEmitter<number>;
+    public valueChange: EventEmitter<number>;
 
-    private _maximum:number;
+    private _maximum: number;
 
     @Input()
-    public get maximum():number {
+    public get maximum(): number {
         return this._maximum;
     }
 
-    public set maximum(value:number) {
+    public set maximum(value: number) {
         this._maximum = +value;
     }
 
-    @HostBinding("class.read-only")
+    @HostBinding('class.read-only')
     @Input()
-    public isReadonly:boolean;
+    public isReadonly: boolean;
 
-    public get icons():undefined[] {
+    public get icons(): undefined[] {
         // tslint:disable-next-line:prefer-literal
         return new Array(this.maximum);
     }
 
-    public hoveredIndex:number = -1;
+    public hoveredIndex: number = -1;
 
     constructor() {
         this.value = 0;
@@ -60,34 +76,38 @@ export class SuiRating implements ICustomValueAccessorHost<number> {
         this.hasClasses = true;
     }
 
-    public onClick(i:number):void {
+    public onClick(i: number): void {
         if (!this.isReadonly) {
-            this.value = i + 1;
+            if (i === 0 && this.value === 1) {
+                this.value = 0;
+            } else {
+                this.value = i + 1;
+            }
             this.valueChange.emit(this.value);
         }
     }
 
-    public onMouseover(i:number):void {
+    public onMouseover(i: number): void {
         this.hoveredIndex = i;
     }
 
-    @HostListener("mouseout")
-    public onMouseout():void {
+    @HostListener('mouseout')
+    public onMouseout(): void {
         this.hoveredIndex = -1;
     }
 
-    public writeValue(value:number):void {
+    public writeValue(value: number): void {
         this.value = value;
     }
 }
 
 @Directive({
-    selector: "sui-rating",
-    host: { "(valueChange)": "onChange($event)" },
+    selector: 'sui-rating',
+    host: { '(valueChange)': 'onChange($event)' },
     providers: [customValueAccessorFactory(SuiRatingValueAccessor)]
 })
 export class SuiRatingValueAccessor extends CustomValueAccessor<number, SuiRating> {
-    constructor(host:SuiRating) {
+    constructor(host: SuiRating) {
         super(host);
     }
 }
