ac88f9c8d3a4fb37979d46fb72b87d4c87a8a929
kfrysiak <konrad.frysiak@gmail.com>
2020-03-20 19:19:31 +0100

    add debounce on search

--- src/modules/select/directives/select-search.ts
+++ src/modules/select/directives/select-search.ts
@@ -1,41 +1,74 @@
-import { Directive, Input, EventEmitter, Renderer2, ElementRef, HostListener, HostBinding } from "@angular/core";
+import {
+    Directive,
+    Input,
+    EventEmitter,
+    Renderer2,
+    ElementRef,
+    HostListener,
+    HostBinding
+} from '@angular/core';
 
 @Directive({
-    selector: "input[suiSelectSearch]"
+    selector: 'input[suiSelectSearch]'
 })
 export class SuiSelectSearch {
-    @HostBinding("class.search")
-    public readonly hasClasses:boolean;
+    @HostBinding('class.search')
+    public readonly hasClasses: boolean;
 
-    @HostBinding("attr.autocomplete")
-    public readonly autoComplete:string;
+    @HostBinding('attr.autocomplete')
+    public readonly autoComplete: string;
 
-    public set query(query:string) {
-        this._renderer.setProperty(this._element.nativeElement, "value", query);
+    public set query(query: string) {
+        this._renderer.setProperty(this._element.nativeElement, 'value', query);
     }
 
-    public onQueryUpdated:EventEmitter<string>;
-    public onQueryKeyDown:EventEmitter<KeyboardEvent>;
+    debounce = (func: (any) => any, wait: number, immediate = false) => {
+        let timeout;
+        return function() {
+            let context = this;
+            let args = arguments;
+            let later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            let callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
 
-    constructor(private _renderer:Renderer2, private _element:ElementRef) {
+    public onQueryUpdated: EventEmitter<string>;
+    public onQueryKeyDown: EventEmitter<KeyboardEvent>;
+
+    debouncedUpdateQuery: (query: string) => void;
+    debouncedKeyDown: (e: KeyboardEvent) => void;
+    constructor(private _renderer: Renderer2, private _element: ElementRef) {
         this.onQueryUpdated = new EventEmitter<string>();
         this.onQueryKeyDown = new EventEmitter<KeyboardEvent>();
 
         this.hasClasses = true;
-        this.autoComplete = "off";
+        this.autoComplete = 'off';
+
+        this.debouncedUpdateQuery = this.debounce((query) => {
+            this.onQueryUpdated.emit(query);
+        }, 300);
+        this.debouncedKeyDown = this.debounce((e) => {
+            this.onQueryKeyDown.emit(e);
+        }, 300);
     }
 
-    @HostListener("input", ["$event.target.value"])
-    public updateQuery(query:string):void {
-        this.onQueryUpdated.emit(query);
+    @HostListener('input', ['$event.target.value'])
+    public updateQuery(query: string): void {
+        this.debouncedUpdateQuery(query);
     }
 
-    @HostListener("keydown", ["$event"])
-    public onKeyDown(e:KeyboardEvent):void {
-        this.onQueryKeyDown.emit(e);
+    @HostListener('keydown', ['$event'])
+    public onKeyDown(e: KeyboardEvent): void {
+        this.debouncedKeyDown(e);
     }
 
-    public focus():void {
+    public focus(): void {
         // Slightly delay to support in menu search.
         this._element.nativeElement.focus();
         setTimeout(() => this._element.nativeElement.focus());
